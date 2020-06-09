a4e0acea6e5532cf906512fcaab373a66815eeb7
unknown <dethgrenade@gmail.com>
2020-06-09 13:31:22 -0700

    get, get by id and post operations added to api and working

--- routes/carsRouter.js
+++ routes/carsRouter.js
@@ -1,7 +1,79 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: "data from db" })
+//connect to the database
+const db = require('../data/connection')
+
+
+//Create a new car entry in the DB
+router.post('/', validateCarData, (req, res) => {
+    db('cars')
+        .insert(req.carData)
+        .then(([id]) => {
+            console.log(id)
+            res.status(201).json(id)
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ errorMessage: error.message })
+        })
 })
+
+//Get a list of cars
+router.get('/', (req, res) => {
+    db('cars')
+        .select('*')
+        .then(cars => {
+            if (cars.length) {
+                res.status(200).json({ data: cars })
+            }
+            else {
+                res.status(404).json({ errorMessage: "No cars list found" })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ errorMessage: error.message })
+        })
+})
+
+//Get a car by ID
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    db('cars')
+        .select('*')
+        .where('id', id)
+        .first()
+        .then(car => {
+            if (car) {
+                res.status(200).json({ data: car })
+            }
+            else {
+                res.status(404).json({ errorMessage: `Car with ID ${id} not found` })
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(500).json({ errorMessage: error.message })
+        })
+})
+
+//Custom middleware
+function validateCarData(req, res, next) {
+    const carData = req.body
+
+    if (
+        carData.vin &&
+        carData.make &&
+        carData.model &&
+        carData.mileage
+    ) {
+        req.carData = req.body
+        next()
+    }
+    else {
+        res.status(400).json({ errorMessage: "Please provide car VIN, Make, Model and Mileage!" })
+    }
+}
 
 module.exports = router
